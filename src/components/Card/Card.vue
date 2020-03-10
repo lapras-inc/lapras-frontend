5a6a5ef90883a2f6bd6a875bab4645df4f579466
minojiro <minojiro@users.noreply.github.com>
2020-03-10 06:26:58 +0000

    [add] Cardコンポーネント

---
+++ src/components/Card/Card.vue
@@ -0,0 +1,22 @@
+<template functional>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Card',
+  functional: true,
+})
+</script>
+
+<style lang="scss">
+.card {
+  background: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0 0 4px 0 #d0d0d0;
+}
+</style>
