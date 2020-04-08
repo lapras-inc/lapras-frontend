624528584f77b0eefdbc02ba111c1e207582d319
minojiro <minojiro@users.noreply.github.com>
2020-04-08 07:42:03 +0000

    [add] Cardコンポーネントに見た目のバリエーションを増やした

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="card" v-bind="context.attrs" v-on="context.listeners">
+  <div
+    :class="`is-skin-${skin}`"
+    class="card"
+    v-bind="context.attrs"
+    v-on="context.listeners"
+  >
     <slot />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, PropType } from '@vue/composition-api'
 
 export default defineComponent({
+  props: {
+    skin: {
+      type: String as PropType<'default' | 'background' | 'background-line'>,
+      default: 'default',
+    },
+  },
   setup(props, context) {
     return {
       context,
@@ -18,8 +29,19 @@ export default defineComponent({
 
 <style lang="scss">
 .card {
-  background: $white;
-  border-radius: $corner-r-l;
-  box-shadow: 0 0 4px 0 #d0d0d0;
+  &.is-skin-default {
+    background: $white;
+    border-radius: $corner-r-l;
+    box-shadow: 0 0 4px 0 #d0d0d0;
+  }
+  &.is-skin-background {
+    border-radius: $corner-r;
+    background: $background;
+  }
+  &.is-skin-background-line {
+    border-radius: $corner-r;
+    background: $background;
+    border: 1px solid $line-color;
+  }
 }
 </style>
