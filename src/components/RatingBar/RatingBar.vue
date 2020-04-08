ebd7425d98c89e8c62fc0ab7800c4a210dafdf30
minojiro <minojiro@users.noreply.github.com>
2020-04-08 06:36:58 +0000

    [add] コンポーネント追加 - RatingBar

---
+++ src/components/RatingBar/RatingBar.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="score-bar" v-bind="context.attrs" v-on="context.listeners">
+    <div
+      class="inner"
+      :style="{
+        width,
+        background,
+      }"
+    ></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    max: {
+      type: Number,
+      default: 5,
+    },
+    value: {
+      type: Number,
+      default: 0,
+    },
+    barColor: {
+      type: String,
+      default: 'linear-gradient(90deg, #171c60 0%, #009ee8 100%)',
+    },
+    highThreshold: {
+      type: Number,
+      default: Infinity,
+    },
+    highBarColor: {
+      type: String,
+      default: '#ff5a5f',
+    },
+  },
+  setup(props, context) {
+    const isHigh = props.value >= props.highThreshold
+    const background = isHigh ? props.highBarColor : props.barColor
+    const width = `${(props.value / props.max) * 100}%`
+    return {
+      background,
+      width,
+      context,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.score-bar {
+  background: $ultra-light-gray;
+}
+
+@keyframes innerAnim {
+  0% {
+    transform: scaleX(0);
+  }
+  100% {
+    transform: scaleX(1);
+  }
+}
+
+.inner {
+  height: 4px;
+  animation: innerAnim 1.2s ease 0s forwards;
+  transform-origin: left top;
+}
+</style>
