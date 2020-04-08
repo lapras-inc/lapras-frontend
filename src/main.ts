0efe7be8d3397e39af008d94e99330775f70f357
minojiro <minojiro@users.noreply.github.com>
2020-04-08 08:13:53 +0000

    [add] コンポーネントをexportする

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,31 @@
+import AvatarImg from '@/components/AvatarImg/AvatarImg.vue'
 import Card from '@/components/Card/Card.vue'
+import CheckBox from '@/components/CheckBox/CheckBox.vue'
+import EnhancedIconButton from '@/components/EnhancedIconButton/EnhancedIconButton.vue'
+import FieldGroup from '@/components/FieldGroup/FieldGroup.vue'
+import FlatButton from '@/components/FlatButton/FlatButton.vue'
 import Icon from '@/components/Icon/Icon.vue'
+import Modal from '@/components/Modal/Modal.vue'
+import RatingBar from '@/components/RatingBar/RatingBar.vue'
+import SelectBox from '@/components/SelectBox/SelectBox.vue'
+import TagLabel from '@/components/TagLabel/TagLabel.vue'
+import TextInput from '@/components/TextInput/TextInput.vue'
+import ToggleLabelSet from '@/components/ToggleLabelSet/ToggleLabelSet.vue'
+import ToggleSwitch from '@/components/ToggleSwitch/ToggleSwitch.vue'
 
 export {
+  AvatarImg,
   Card,
+  CheckBox,
+  EnhancedIconButton,
+  FieldGroup,
+  FlatButton,
   Icon,
+  Modal,
+  RatingBar,
+  SelectBox,
+  TagLabel,
+  TextInput,
+  ToggleLabelSet,
+  ToggleSwitch,
 }
