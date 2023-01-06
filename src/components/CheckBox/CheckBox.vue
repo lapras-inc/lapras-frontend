6c25d2451b3397bf9b7e14adcfe9d16da554f4be
Masahiko Shin <contact@shin-translation.com>
2023-01-06 16:57:28 +0900

    feat(vue3): v-modelの仕様変更対応
    valueではなくmodelValue, inputイベントではなくupdate:modelValueになったため。使用側でも対応が必要な場合があるので気をつける

--- src/components/CheckBox/CheckBox.vue
+++ src/components/CheckBox/CheckBox.vue
@@ -2,7 +2,7 @@
   <label class="check-box" :class="{ 'is-disabled': disabled }">
     <input
       v-bind="context.attrs"
-      :checked="value"
+      :checked="modelValue"
       @change="onInput"
       class="input"
       type="checkbox"
@@ -22,7 +22,7 @@ export default defineComponent({
     Icon,
   },
   props: {
-    value: {
+    modelValue: {
       type: Boolean,
       default: false,
     },
@@ -32,12 +32,16 @@ export default defineComponent({
     },
   },
   emits: {
-    input: null,
+    'update:modelValue': null,
   },
   setup(_, context) {
-    const emitInput = (value: boolean) => context.emit('input', value)
-    const onInput = (e: Event) =>
+    const emitInput = (value: boolean) =>
+      context.emit('update:modelValue', value)
+
+    const onInput = (e: Event) => {
       emitInput((e.target as HTMLInputElement).checked)
+    }
+
     return {
       context,
       onInput,
