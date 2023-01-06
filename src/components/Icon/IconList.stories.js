dc81bf9bdc894926b2af434e55716857d7329002
Masahiko Shin <contact@shin-translation.com>
2023-01-06 15:03:32 +0900

    feat(storybook): storybookのVue3対応

--- src/components/Icon/IconList.stories.js
+++ src/components/Icon/IconList.stories.js
@@ -1,10 +1,10 @@
-import { storiesOf } from '@storybook/vue'
+import { storiesOf } from '@storybook/vue3'
 import iconMap from './iconMap'
 import Icon from './Icon.vue'
 
 const story = storiesOf('Icon/icons', module)
 
-Object.keys(iconMap).forEach(name => {
+Object.keys(iconMap).forEach((name) => {
   story.add(name, () => ({
     template: `<span style="font-size: 50px;"><Icon name="${name}" /></span>`,
     components: { Icon },
