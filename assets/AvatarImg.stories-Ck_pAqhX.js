4fdf4069d7b3bf517c70c45d545f8598f1f48d0e
GitHub Action <action@github.com>
2024-11-12 04:05:41 +0000

    Updates

---
+++ assets/AvatarImg.stories-Ck_pAqhX.js
@@ -0,0 +1,11 @@
+import{d as g,o as d,f as i,m as u}from"./vue.esm-bundler-DZRSKMuI.js";import{_ as f}from"./_plugin-vue_export-helper-DlAUqK2U.js";const v="data:image/svg+xml;charset=utf8,%3Csvg%20height%3D%22200%22%20viewBox%3D%220%200%20200%20200%22%20width%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cg%20fill%3D%22none%22%20fill-rule%3D%22evenodd%22%3E%3Cpath%20d%3D%22m0%200h200v200h-200z%22%20fill%3D%22%23D7D9E3%22%2F%3E%3Cg%20fill%3D%22%23888A93%22%3E%3Ccircle%20cx%3D%22100%22%20cy%3D%2271%22%20r%3D%2253%22%2F%3E%3Cpath%20d%3D%22m179%20200c0-37.555363-35.369505-68-79-68-43.6304952%200-79%2030.444637-79%2068s158%2037.555363%20158%200z%22%2F%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",l=g({inheritAttrs:!1,props:{src:{type:String}},setup(){return{avatarDefault:v}}});function D(e,h,E,_,F,I){return d(),i("div",u(e.$attrs,{style:{backgroundImage:`url('${e.src||e.avatarDefault}')`},class:"avatar-icon",role:"img"}),null,16)}const s=f(l,[["render",D],["__scopeId","data-v-ae8d1844"]]);l.__docgenInfo={exportName:"default",displayName:"AvatarImg",description:"",tags:{},props:[{name:"src",type:{name:"string"}}],sourceFiles:["/home/runner/work/lapras-frontend/lapras-frontend/src/components/AvatarImg/AvatarImg.vue"]};const A={title:"AvatarImg",component:s,render:e=>({components:{AvatarImg:s},setup(){return{args:e}},template:'<AvatarImg :src="args.src" style="width: 50px"/>'})},a={name:"default",args:{src:"//placehold.jp/50x50.png"}},r={name:"no image",args:{src:""}};var t,n,o;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  name: 'default',
+  args: {
+    src: '//placehold.jp/50x50.png'
+  }
+}`,...(o=(n=a.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};var c,m,p;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  name: 'no image',
+  args: {
+    src: ''
+  }
+}`,...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const w=["Default","NoImage"];export{a as Default,r as NoImage,w as __namedExportsOrder,A as default};
